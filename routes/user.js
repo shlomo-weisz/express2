b5e47252a6d315141f34471a3bbf02af9df73f62
shlomo-weisz <a6169449@gmail.com>
2023-11-13 20:14:27 +0200

    first

--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,10 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const path = require('path');
+const {getUser} = require('../controllers/userController');
 
 // GET /user
-router.get('/', (req, res) => {
-	res.send('Hello World! user');
-});
+router.get('/', getUser);
 
 router.post('/', (req, res) => {
 	res.send('Got a POST request');
